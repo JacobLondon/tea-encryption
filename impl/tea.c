6289f0b84c4f9160fd22b910a72798ecf50c1552
Jacob London <jelondon12@gmail.com>
2019-11-18 15:47:55 -0800

    started state machine impl

--- impl/tea.c
+++ impl/tea.c
@@ -5,10 +5,12 @@ static void encipher (unsigned int rounds, uint32_t v[2], uint32_t k[4]) {
     uint32_t delta=0x9E3779B9;                     /* a key schedule constant */
     uint32_t v0=v[0], v1=v[1], sum=0, i;           /* set up */
     uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
+    //printf("%x\t%x\n", v0, v1);
     for (i=0; i<rounds; i++) {                         /* basic cycle start */
         sum += delta;
         v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
         v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
+        //printf("%x\t%x\n", v0, v1);
     }                                              /* end cycle */
     v[0]=v0; v[1]=v1;
 }
