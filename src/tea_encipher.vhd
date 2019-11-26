2a3f0423bda8d3d4f50a27308b984d6b80be5641
Jacob London <jelondon12@gmail.com>
2019-11-25 16:06:37 -0800

    additional work on encode

--- src/tea_encipher.vhd
+++ src/tea_encipher.vhd
@@ -10,7 +10,8 @@ entity TeaEncipher is
         num_rounds  : in  UNSIGNED(7 downto 0);
         input_data  : in  UNSIGNED(63 downto 0);
         key         : in  UNSIGNED(127 downto 0);
-        output_data : out UNSIGNED(63 downto 0)
+        output_data : out UNSIGNED(63 downto 0);
+        done        : out STD_LOGIC
     );
 end TeaEncipher;
 
@@ -18,17 +19,15 @@ architecture Behavioral of TeaEncipher is
     
     signal delta : UNSIGNED(31 downto 0);
     signal k0, k1, k2, k3 : UNSIGNED(31 downto 0);
-    signal v0, v1 : UNSIGNED(31 downto 0);
-    signal sum : UNSIGNED(31 downto 0);
+    signal v0, v1, v0_new, v1_new, t1, t2, t3, t4,t5, t6, t7 : UNSIGNED(31 downto 0);
+    signal sum, next_sum : UNSIGNED(31 downto 0);
     
-    signal round : UNSIGNED(7 downto 0);
-    signal state : UNSIGNED(3 downto 0) := "0000";
-    signal result : UNSIGNED(63 downto 0);
+    signal round, next_round: UNSIGNED(7 downto 0);
+    type STATE_TYPE is (s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17, s18, s19, s20);
+    signal curr_state, next_state : STATE_TYPE;
 
 begin
 
-    --matrix(0)   <= input_data;
-    --output_data <= matrix(g_ROUNDS);
     delta <= x"9E3779B9";
     
     k0 <= key(31 downto 0);
@@ -36,49 +35,85 @@ begin
     k2 <= key(95 downto 64);
     k3 <= key(127 downto 96);
     
-    ENCIPHER_MACHINE: process (clk) is
+    CONTINUE: process (clk)
     begin
         if rising_edge(clk) then
-            case state is
-                -- initialize, only happen once
-                when x"0" => 
-                    sum <= x"00000000";
-                    round <= x"00";
-                    v0  <= input_data(31 downto 0);
-                    v1  <= input_data(63 downto 32);
-                    state <= x"1";
-                when x"1" => 
-                when x"2" => 
-                when x"3" => 
-                when x"4" => 
-                when x"5" => 
-                when x"6" => 
-                when x"7" => 
-                when x"8" => 
-                when x"9" => 
-                when x"A" => 
-                when x"B" => 
-                when x"C" => 
-                when x"D" => 
-                when x"E" => 
-                    -- finished doing all rounds
-                    if round + 1 = num_rounds then
-                        state <= x"F";
-                    -- go again for another round
-                    else
-                        round <= round + 1;
-                        state <= x"1";
-                    end if;
-                -- close down, wait for rst signal
-                when x"F" => 
-                    if rst = '1' then
-                        state <= x"0";
-                    else
-                        state <= x"F";
-                    end if;
-                when others => state <= x"0";
-            end case;
+            if rst = '1' then
+                curr_state <= s0;
+            else
+                curr_state <= next_state;
+            end if;
         end if;
+    end process;
+    
+    ENCIPHER_MACHINE: process (clk)
+    begin
+        case curr_state is
+            -- reset all variables
+            when s0 => 
+                done   <= '0';
+                sum    <= (others => '0');
+                next_sum <= (others => '0');
+                round  <= (others => '0');
+                next_round <= (others => '0');
+                t1     <= (others => '0');
+                t2     <= (others => '0');
+                t3     <= (others => '0');
+                t4     <= (others => '0');
+                t5     <= (others => '0');
+                t6     <= (others => '0');
+                t7     <= (others => '0');
+                v0     <= input_data(31 downto 0);
+                v1     <= input_data(63 downto 32);
+                v0_new <= (others => '0');
+                v1_new <= (others => '0');
+                next_state <= s1;
+            when s1 => 
+                next_state <= s2;
+            when s2 => 
+                next_state <= s3;
+            when s3 => 
+                next_state <= s4;
+            when s4 => 
+                next_state <= s5;
+            when s5 => 
+                next_state <= s6;
+            when s6 => 
+                next_state <= s7;
+            when s7 => 
+                next_state <= s8;
+            when s8 => 
+                next_state <= s9;
+            when s9 => 
+                next_state <= s10;
+            when s10 => 
+                next_state <= s11;
+            when s11 => 
+                next_state <= s12;
+            when s12 => 
+                next_state <= s13;
+            when s13 => 
+                next_state <= s14;
+            when s14 => 
+                next_state <= s15;
+            when s15 => 
+                t6 <= t2 xor t3;
+                next_state <= s16;
+            when s16 => 
+                next_state <= s17;
+            when s17 => 
+                next_state <= s18;
+            when s18 => 
+                next_state <= s19;
+            when s19 => 
+            when s20 => 
+            when s21 => 
+            when s22 => 
+            when s23 => 
+            when s24 => 
+            when others => 
+                next_state <= s0;
+        end case;
     end process;
 
 end Behavioral;
--- src/tea_encipher_tb.vhd
+++ src/tea_encipher_tb.vhd
@@ -8,33 +8,64 @@ end TeaEncipherTb;
 
 architecture Behavioral of TeaEncipherTb is
 
-    component TeaEncipher is
-        Generic (
-            g_ROUNDS : POSITIVE := 64
-        );
+    component TeaEncipher
         Port (
+            clk         : in  STD_LOGIC;
+            rst         : in  STD_LOGIC;
+            num_rounds  : in  UNSIGNED(7 downto 0);
             input_data  : in  UNSIGNED(63 downto 0);
             key         : in  UNSIGNED(127 downto 0);
-            output_data : out UNSIGNED(63 downto 0)
+            output_data : out UNSIGNED(63 downto 0);
+            done        : out STD_LOGIC
         );
-    end component;
+    end component;    
+    
+    constant jump : time := 10 ns;
+    signal clk, rst, done : STD_LOGIC;
+    
+    signal num_rounds : UNSIGNED(7 downto 0);
 
     signal din, dout : UNSIGNED(63 downto 0);
     signal key       : UNSIGNED(127 downto 0);
     
 begin
-
-    din <= x"DEADBEEFFEEBDAED";
-    key <= x"FEEDBEEF00C0FFEEF00000110FACADE0";
     
-    ENCODER: TeaEncipher
-        generic map (
-            g_ROUNDS => 64
-        )
+    CLKGEN: process
+    begin
+        clk <= '1';
+        wait for jump;
+        clk <= '0';
+        wait for jump;
+    end process;
+    
+    UUT: TeaEncipher
         port map (
+            clk         => clk,
+            rst         => rst,
+            num_rounds  => num_rounds,
             input_data  => din,
             key         => key,
-            output_data => dout
+            output_data => dout,
+            done        => done
         );
+        
+    TESTBENCH: process
+    begin
+        din <= x"DEADBEEFFEEBDAED";
+        key <= x"FEEDBEEF00C0FFEEF00000110FACADE0";
+        num_rounds <= x"40";
+        
+        rst <= '1';
+        wait for jump;
+        rst <= '0';
+        wait for jump;
+        
+        while done = '0' loop
+            wait for jump;
+        end loop;
+        
+        wait;
+    
+    end process;
 
 end Behavioral;
